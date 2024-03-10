fd74e3b0bffb59981e8e5847bc3e3f9d5007ee6a
Tabasoume <145778331+Tboumpsh@users.noreply.github.com>
2024-03-11 01:54:47 +0330

    [create]-create page level
    Description:
    Create pages for level of game

--- asset/scripts/script.js
+++ asset/scripts/script.js
@@ -4,3 +4,15 @@ easy.addEventListener('click' , ()=> {
    window.open("secendex.html", "_blank");
 })
 
+
+let normal = document.getElementById('normal')
+normal.addEventListener('click' , ()=> {
+   window.open("theardex.html", "_blank");
+})
+
+
+let difficult = document.getElementById('difficult')
+difficult.addEventListener('click' , ()=> {
+   window.open("foursex.html", "_blank");
+})
+
